72e3637f300010f345fb8b79f7961081381de100
Afliday <aflidaywork@gmail.com>
2026-03-03 16:53:58 +0300

    C:/Program Files/Git/videos

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ const port = process.env.PORT || 5000
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello Samurai')
 })
+app.get("/videos", (req: Request, res: Response) => {
+        res.status(200).send(db.videos);
+    });
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
