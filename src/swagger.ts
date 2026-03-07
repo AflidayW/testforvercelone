4c539572b10b15baf921240768b2294c6c396acf
Afliday <aflidaywork@gmail.com>
2026-03-07 14:33:44 +0300

    ff

--- src/swagger.ts
+++ src/swagger.ts
@@ -13,71 +13,69 @@ const swaggerOptions = {
     servers: [
       { url: "https://testforvercelone.vercel.app/", },
 
-      { url: "http://localhost:5000", }
+        { url: "http://localhost:5000", }
 
-    ],
+      ],
 
-    paths: {
-      "/videos": {
-        get: {
-          summary: "Get a list of all videos",
-          tags: ["Videos"],
-          responses: {
-            200: { description: "List of all videos" }
+      paths: {
+        "/videos": {
+          get: {
+            summary: "Get a list of all videos",
+            tags: ["Videos"],
+            responses: {
+              200: { description: "List of all videos" }
+            }
+          },
+          post: {
+            summary: "Create a new video",
+            tags: ["Videos"],
+            responses: {
+              201: { description: "Video created" },
+              400: { description: "Validation error" }
+            }
           }
         },
-        post: {
-          summary: "Create a new video",
-          tags: ["Videos"],
-          responses: {
-            201: { description: "Video created" },
-            400: { description: "Validation error" }
-          }
-        }
-      },
-      "/videos/{id}": {
-        get: {
-          summary: "Get video by ID",
-          tags: ["Videos"],
-          parameters: [{ in: "path", name: "id", required: true, schema: { type: "integer" } }],
-          responses: {
-            200: { description: "Video found" },
-            404: { description: "Video Not Found" }
-          }
-        },
-        put: {
-          summary: "Update video by ID",
-          tags: ["Videos"],
-          parameters: [{ in: "path", name: "id", required: true, schema: { type: "integer" } }],
-          responses: {
-            204: { description: "Video updated" },
-            400: { description: "Validation error" },
-            404: { description: "Video Not Found" }
-          }
-        },
-        delete: {
-          summary: "Delete video by ID",
-          tags: ["Videos"],
-          parameters: [{ in: "path", name: "id", required: true, schema: { type: "integer" } }],
-          responses: {
-            204: { description: "Video deleted" },
-            404: { description: "Video Not Found" }
+        "/videos/{id}": {
+          get: {
+            summary: "Get video by ID",
+            tags: ["Videos"],
+            parameters: [{ in: "path", name: "id", required: true, schema: { type: "integer" } }],
+            responses: {
+              200: { description: "Video found" },
+              404: { description: "Video Not Found" }
+            }
+          },
+          put: {
+            summary: "Update video by ID",
+            tags: ["Videos"],
+            parameters: [{ in: "path", name: "id", required: true, schema: { type: "integer" } }],
+            responses: {
+              204: { description: "Video updated" },
+              400: { description: "Validation error" },
+              404: { description: "Video Not Found" }
+            }
+          },
+          delete: {
+            summary: "Delete video by ID",
+            tags: ["Videos"],
+            parameters: [{ in: "path", name: "id", required: true, schema: { type: "integer" } }],
+            responses: {
+              204: { description: "Video deleted" },
+              404: { description: "Video Not Found" }
+            }
           }
         }
       }
-    }
-  },
-  apis: ["./src/routes/*.ts"],
-};
+    },
+    apis: ["./src/routes/*.ts"],
+  };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 export const setupSwagger = (app: Express) => {
-  app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
-    swaggerOptions: {
-      url: "/api/swagger.json"
-    }
-  }));
+    app.get("/api/swagger.json", (req, res) => {
+      res.json(swaggerSpec);
+    });
 
   app.get("/api", (req, res) => {
     res.send(`
@@ -101,3 +99,10 @@ export const setupSwagger = (app: Express) => {
     `);
   });
 };
+
+
+// // app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+// //   swaggerOptions: {
+// //     url: "/api/swagger.json"
+// //   }
+// }));
