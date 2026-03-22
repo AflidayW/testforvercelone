ad88b1cfbc75ee9b20cc4c834c32256ef580aa14
Afliday <aflidaywork@gmail.com>
2026-03-22 16:58:26 +0300

    blogname

--- src/middlewares/IdBlogvalidator.ts
+++ src/middlewares/IdBlogvalidator.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { db } from "../db";
 export const idBlogValidation =async (req: Request, res: Response, next: NextFunction) => {
 
-    const id = await db.collection("Posts").findOne({ id: req.params.id })
+    const id = await db.collection("Blogs").findOne({ id: req.params.id })
 
     if (!id) {
         return res.status(404).json({ message: 'Blog not found' });
