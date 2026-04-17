159aaea38a30ac9753abfeb7a0a92325efeffc06
Afliday <aflidaywork@gmail.com>
2026-04-17 17:58:30 +0300

    crashed

--- src/middlewares/sortvalidation.ts
+++ src/middlewares/sortvalidation.ts
@@ -13,19 +13,19 @@ export const sortValidator = [
         .default(PostDirectionEnum.DESC)
         .isIn(Object.values(PostDirectionEnum))
         .withMessage(`Allowed sort fields: ${Object.values(PostDirectionEnum).join(', ')}`),
-    query('pageNumber')
-        .optional()
-        .default(1)
-        .isInt({ min: 1 })
-        .withMessage('pageNumber must be a positive integer')
-        .toInt(),
-    
-    query('pageSize')
-        .optional()
-        .default(10)
-        .isInt({ min: 1 })
-        .withMessage('pageSize must be a positive integer')
-        .toInt(),
+    // query('pageNumber')
+    //     .optional()
+    //     .default(1)
+    //     .isInt({ min: 1 })
+    //     .withMessage('pageNumber must be a positive integer')
+    //     .toInt(),
+
+    // query('pageSize')
+    //     .optional()
+    //     .default(10)
+    //     .isInt({ min: 1 })
+    //     .withMessage('pageSize must be a positive integer')
+    //     .toInt(),
 
 ]
 
