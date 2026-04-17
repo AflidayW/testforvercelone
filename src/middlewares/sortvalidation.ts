497deb4b200b003c4e4f6d91b582d97703dd4626
Afliday <aflidaywork@gmail.com>
2026-04-17 17:42:21 +0300

    sort updated

--- src/middlewares/sortvalidation.ts
+++ src/middlewares/sortvalidation.ts
@@ -4,14 +4,28 @@ import { PostSortFieldEnum, PostDirectionEnum } from '..//middlewares/main.enums
 
 export const sortValidator = [
     query('sortBy')
+        .optional()
         .default(PostSortFieldEnum.CREATED_AT)
         .isIn(Object.values(PostSortFieldEnum))
         .withMessage(`Allowed sort fields: ${Object.values(PostSortFieldEnum).join(', ')}`),
     query("sortDirection")
+        .optional()
         .default(PostDirectionEnum.DESC)
         .isIn(Object.values(PostDirectionEnum))
-        .withMessage(`Allowed sort fields: ${Object.values(PostDirectionEnum).join(', ')}`)
+        .withMessage(`Allowed sort fields: ${Object.values(PostDirectionEnum).join(', ')}`),
+    query('pageNumber')
+        .optional()
+        .default(1)
+        .isInt({ min: 1 })
+        .withMessage('pageNumber must be a positive integer')
+        .toInt(),
     
+    query('pageSize')
+        .optional()
+        .default(10)
+        .isInt({ min: 1 })
+        .withMessage('pageSize must be a positive integer')
+        .toInt(),
 
 ]
 
